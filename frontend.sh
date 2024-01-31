878c2d4938e4f35a62e19bf49803e6e216a4bf94
durgapraveen129 <durgapraveenlankalapallis@gmail.com>
2024-01-31 16:57:07 +0530

    commit it

--- frontend.sh
+++ frontend.sh
@@ -1,16 +1,16 @@
 echo -e "${color}installing nginx${nocolor}"
 yum install nginx -y &>>/tmp/roboshop.log
 
-echo -e "${color}installing nginx${nocolor}"
+echo -e "${color}removing old directories content${nocolor}"
 rm -rf /usr/share/nginx/html/ &>>/tmp/roboshop.log
 
-echo -e "${color}installing nginx${nocolor}"
+echo -e "${color}copying the directories${nocolor}"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
 
-echo -e "${color}installing nginx${nocolor}"
+echo -e "${color}unzipping directories${nocolor}"
 cd /usr/share/nginx/html &>>/tmp/roboshop.log
 unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 
-echo -e "${color}installing nginx${nocolor}"
+echo -e "${color}starting the service${nocolor}"
 systemctl enable nginx
 systemctl restart nginx &>>/tmp/roboshop.log
